423536104835491bc3802ee8e1891981a0041624
Paul Campbell <paul@rslw.com>
2008-09-25 16:12:48 +0100

    make the link work

--- twitter2campfire.rb
+++ twitter2campfire.rb
@@ -17,7 +17,7 @@ class Twitter2Campfire
   end
   
   def entries
-    (raw_feed/'entry').map { |e| OpenStruct.new(:from => (e/'name').inner_html, :text => (e/'title').inner_html, :link => (e/'link').inner_html) }
+    (raw_feed/'entry').map { |e| OpenStruct.new(:from => (e/'name').inner_html, :text => (e/'title').inner_html, :link => (e/'link').first['href']) }
   end
   
   def latest_tweet
@@ -37,9 +37,9 @@ class Twitter2Campfire
   end
   
   def publish_entries
-    entries.each do |entry|
+    entries.reverse.each do |entry|
       return if entry.text == archived_latest
-      room.speak "#{entry.from} #{entry.text} #{entry.link}"
+      room.speak "#{entry.from}: #{entry.text} #{entry.link}"
     end
     save_latest
   end
--- twitter2campfire_spec.rb
+++ twitter2campfire_spec.rb
@@ -5,6 +5,7 @@ require 'twitter2campfire'
 describe Twitter2Campfire do
   before(:each) do
     campfire = mock(Tinder::Campfire)
+    campfire.stub!(:find_room_by_name)
     @t = Twitter2Campfire.new('http://search.twitter.com/search.atom?q=from%3Apaulca+OR+from%3Aeoghanmccabe+OR+from%3Adestraynor+OR+from%3Adavidjrice', campfire, 'Contrast')
   end
   
@@ -12,7 +13,7 @@ describe Twitter2Campfire do
     @t.latest_tweet.should == 'stuff'
   end
   
-  it "should get the latest tweet" do
-    @t.latest_tweet.should == 'stuff'
+  it "should get the latest entry" do
+    (@t.raw_feed/'entry').first.should == 'stuff'
   end
 end
