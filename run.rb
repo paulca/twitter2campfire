4cd5515f9b431a83c0f4be7c50bfd322736c6701
Paul Campbell <paul@rslw.com>
2008-10-10 13:40:03 +0100

    add a few comments

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
 CAMPFIRE_SUBDOMAIN = ''
 CAMPFIRE_EMAIL = ''
-CAMPFIRE_PASSWORD = ''
-CAMPFIRE_ROOM = ''
-FEED_URL = ''
+CAMPFIRE_PASSWORD = '' 
+CAMPFIRE_ROOM = '' # the NAME of the room, case sensitive
+FEED_URL = '' # from http://search.twitter.com
 
 require 'twitter2campfire'
 campfire = Tinder::Campfire.new CAMPFIRE_SUBDOMAIN
