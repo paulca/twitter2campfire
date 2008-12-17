59cb0a17fd55e4a12b19aca37213018c9af80c18
Paul Campbell <paul@rslw.com>
2008-12-17 12:58:01 +0000

    and we're good

--- twitter2campfire.rb
+++ twitter2campfire.rb
@@ -5,6 +5,7 @@ require 'hpricot'
 require 'ostruct'
 require 'time'
 require 'htmlentities'
+require 'digest/sha1'
 
 class Twitter2Campfire
   attr_accessor :feed, :campfire, :room, :cachefile
@@ -20,7 +21,7 @@ class Twitter2Campfire
   end
   
   def entries
-    (raw_feed/'entry').map { |e| OpenStruct.new(:from => (e/'name').inner_html, :text => (e/'title').inner_html, :link => (e/'link[@rel=alternate]').first['href'], :date => Time.parse((e/'updated').inner_html)) }
+    (raw_feed/'entry').map { |e| OpenStruct.new(:from => (e/'name').inner_html, :text => (e/'title').inner_html, :link => (e/'link[@rel=alternate]').first['href'], :checksum => Digest::SHA1.hexdigest((e/'title').inner_html), :date => Time.parse((e/'updated').inner_html)) }
   end
   
   def latest_tweet
@@ -32,17 +33,29 @@ class Twitter2Campfire
     archive_file << latest_tweet.date.to_s
   end
   
+  def checksums
+    entries.map { |e| e.checksum }.to_a
+  end
+  
+  def archived_checksums
+    archive_file.split("\n")[1,archive_file.length].to_a
+  end
+  
+  def new_checksums
+    [checksums + archived_checksums].flatten.uniq[0,1000]
+  end
+  
   def archive_file
-    rio(cachefile)
+    File.read(cachefile)
   end
   
   def archived_latest_date
-    archive_file >> (string ||= "")
-    Time.parse(string)
+    time = archive_file.split("\n")[0] ? archive_file.split("\n")[0] : '1 january 1970 00:00'
+    Time.parse(time)
   end
   
   def posts
-    entries.reject { |e| e.date.to_i <= archived_latest_date.to_i }
+    entries.reject { |e| e.date.to_i <= archived_latest_date.to_i or archived_checksums.include?(e.checksum) }
   end
   
   def coder
--- twitter2campfire_spec.rb
+++ twitter2campfire_spec.rb
@@ -2,23 +2,100 @@ require 'rubygems'
 require 'spec'
 require 'twitter2campfire'
 
-describe Twitter2Campfire do
+describe Twitter2Campfire, "with a cachefile" do
   before(:each) do
     campfire = mock(Tinder::Campfire)
     campfire.stub!(:find_room_by_name)
-    @t = Twitter2Campfire.new('http://search.twitter.com/search.atom?q=from%3Apaulca+OR+from%3Aeoghanmccabe+OR+from%3Adestraynor+OR+from%3Adavidjrice', campfire, 'Contrast')
+    @t = Twitter2Campfire.new('http://search.twitter.com/search.atom?q=from%3Apaulca+OR+from%3Aeoghanmccabe+OR+from%3Adestraynor+OR+from%3Adavidjrice', campfire, 'Contrast', 'fixtures/test_cachefile.txt')
+    @t.stub!(:raw_feed).and_return(Hpricot(File.open('fixtures/sample.xml')))
   end
   
   it "should get the latest tweet" do
-    @t.latest_tweet.should == 'stuff'
+    @t.latest_tweet.text.should match(/svenfuchs/)
   end
   
   it "should get the latest entry" do
-    (@t.raw_feed/'entry').first.should == 'stuff'
+    ((@t.raw_feed/'entry').first/'title').first.inner_html.should match(/svenfuchs/)
   end
   
   it "should get the archived latest" do
-    @t.archived_latest_date.should == 'test'
+    @t.archived_latest_date.should == Time.parse('19 January 1990 10:20')
   end
   
+  it "should get the archived checksums" do
+    @t.archived_checksums.should == ['be7bb22cc88ff98bfd4ffe3c45ad89a7f3f4e86a', '81d405cd1e3b4bb6c959419e57c2a5c7abe688cb']
+  end
+  
+  it "should have all the entries" do
+    @t.entries.size.should == 15
+  end
+  
+  it "have the first checksum" do
+    @t.checksums.first.should == 'be7bb22cc88ff98bfd4ffe3c45ad89a7f3f4e86a'
+  end
+  
+  it "have the last checksum" do
+    @t.checksums.last.should == '81d405cd1e3b4bb6c959419e57c2a5c7abe688cb'
+  end
+  
+  it "should have 15 checksums" do
+    @t.checksums.size.should == 15
+  end
+  
+  it "should only have 13 posts" do
+    @t.posts.size.should == 13
+  end
+  
+  it "should have a bunch of new checksums" do
+    @t.new_checksums.size.should == 15
+  end
+end
+
+describe Twitter2Campfire, "with a blank cachefile" do
+  before(:each) do
+    campfire = mock(Tinder::Campfire)
+    campfire.stub!(:find_room_by_name)
+    @t = Twitter2Campfire.new('http://search.twitter.com/search.atom?q=from%3Apaulca+OR+from%3Aeoghanmccabe+OR+from%3Adestraynor+OR+from%3Adavidjrice', campfire, 'Contrast', 'fixtures/test_blank_cachefile.txt')
+    @t.stub!(:raw_feed).and_return(Hpricot(File.open('fixtures/sample.xml')))
+  end
+  
+  it "should get the latest tweet" do
+    @t.latest_tweet.text.should match(/svenfuchs/)
+  end
+  
+  it "should get the latest entry" do
+    ((@t.raw_feed/'entry').first/'title').first.inner_html.should match(/svenfuchs/)
+  end
+  
+  it "should get the archived latest" do
+    @t.archived_latest_date.to_date.should == Date.parse('1 jan 1970')
+  end
+  
+  it "should get the archived checksums" do
+    @t.archived_checksums.should == []
+  end
+  
+  it "should have all the entries" do
+    @t.entries.size.should == 15
+  end
+  
+  it "have the first checksum" do
+    @t.checksums.first.should == 'be7bb22cc88ff98bfd4ffe3c45ad89a7f3f4e86a'
+  end
+  
+  it "have the last checksum" do
+    @t.checksums.last.should == '81d405cd1e3b4bb6c959419e57c2a5c7abe688cb'
+  end
+  
+  it "should have 15 checksums" do
+    @t.checksums.size.should == 15
+  end
+  
+  it "should only have 13 posts" do
+    @t.posts.size.should == 15
+  end
+  
+  it "should have a bunch of new checksums" do
+    @t.new_checksums.size.should == 15
+  end
 end
